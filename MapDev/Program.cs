d4f96b5208f62ea6011bbf8f6e1685a2bd7967a0
Peter Yuan <Peter.Yuan@Tm-robot.com>
2025-07-22 15:14:51 +0800

    加入專案檔案。

---
+++ MapDev/Program.cs
@@ -0,0 +1,41 @@
+﻿using MapDev.Components;
+using MapDev.Services;
+using CommonLibraryP.MachinePKG;
+using CommonLibraryP.MapPKG;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddDevExpressBlazor(options =>
+{
+    options.SizeMode = DevExpress.Blazor.SizeMode.Large;
+});
+builder.Services.AddMvc();
+builder.Services.AddScoped<DxThemesService>();
+
+
+builder.AddMachineService("machineConnectionString");
+builder.AddMapService("mapConnectionString");
+
+
+var app = builder.Build();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AllowAnonymous();
+
+app.Run();
